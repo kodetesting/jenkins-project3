5ce21268684f8b7325c13d0a5756cd2f60f81fd1
santosh <santosh.ece06@gmail.com>
2022-08-10 10:36:59 +0530

    INitial Commit.

---
+++ src/main/java/com/example/jenkinsprojec3/JenkinsProjec3Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsprojec3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjec3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjec3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsprojec3/JenkinsProjec3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsprojec3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjec3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
